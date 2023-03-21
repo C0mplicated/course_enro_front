dbe65a674274b551a0100bbf725f2f51eab61de4
C0mplicated <zltb@outlook.com>
2023-03-20 23:04:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import AllCrouses from "./views/AllCrouses";
+import EnrolledCrouses from "./views/EnrolledCrouses";
+import MenuBar from "./components/MenuBar";
+import LoginDialog from "./components/LoginDialog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MenuBar/>
+      <Routes>
+        <Route path="/" element={<AllCrouses />} />
+        <Route path="enrolled-crouses" element={<EnrolledCrouses />} />
+          <Route path="login-dialog" element={<LoginDialog/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
